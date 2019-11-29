e20a056b7954944de55f20526a65a58ec5fd3f51
Jessica Yin <jyin978@hotmail.com>
2019-11-29 11:31:53 -0800

    Added in arrays for stacks
    Using an array for a stack is so much easer.

--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -45,6 +45,28 @@ class Stack  {
     }
 }
 
+//arrays...
+class Stack  {
+    constructor() {
+        this.array = [];
+    }
+    peek() {
+        return this.array[this.array.length-1];
+    }
+    push(value){
+        this.array.push(value);
+        return this;
+    }
+    pop(){
+        this.array.pop();
+        return this;
+        }
+    }
+
+
+
+
+
 const myStack = new Stack();
 myStack.push('google');
 myStack.push('udemy');
