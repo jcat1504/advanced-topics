01e39d76c8a4449d7e009241c292833b55d18480
Jessica Yin <jyin978@hotmail.com>
2019-11-29 12:03:59 -0800

    CrazyQueue-
    Solution to implementing a queue using stacks

---
+++ crazyQueue.js
@@ -0,0 +1,48 @@
+// Implement the following operations of a queue using stacks.
+
+// push(x) -- Push element x to the back of queue.
+// pop() -- Removes the element from in front of queue.
+// peek() -- Get the front element.
+// empty() -- Return whether the queue is empty.
+
+class CrazyQueue {
+    constructor() {
+        this.first = [];
+        this.last = [];
+    }
+
+    enqueue(value) {
+        const length = this.first.length;
+        for (let i=0; i < length; i++) {
+            this.last.push(this.first.pop());
+        }
+        this.last.push(value);
+        return this;
+    }
+
+    dequeue() {
+        const length = this.last.length;
+        for (let i = 0; i < length; i++) {
+            this.first.push(this.last.pop());
+        }
+        this.first.pop();
+        return this;
+    }
+    peek() {
+        if(this.last.length > 0) {
+            return this.last[0]
+        }
+        return this.first[this.first.length-1];
+    }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
--- stacksAndQueues.js
+++ stacksAndQueues.js
@@ -129,33 +129,5 @@ myQueue.enqueue('Pavel');
 myQueue.enqueue('Samir');
 myQueue.dequeue('Joy');
 
-//Arrays O(n)
-class Stack  {
-    constructor() {
-        this.array = [];
-    }
-    peek() {
-        return this.array[this.array.length-1];
-    }
-    push(value){
-        this.array.push(value);
-        return this;
-    }
-    pop(){
-        this.array.pop();
-        return this;
-        }
-    }
-
-
-
-
-
-const myStack = new Stack();
-myStack.push('google');
-myStack.push('udemy');
-myStack.push('discord');
-myStack.pop();
-myStack.pop();
 //LinkedLists <-- Better! O(1) constant time
 
