d0be2998c898c84f68eaa56efe3a666643bfb620
Jessica Yin <jyin978@hotmail.com>
2019-12-04 08:59:02 -0800

    Create factorialRecursion.js

---
+++ factorialRecursion.js
@@ -0,0 +1,16 @@
+function findFactorialRecursive(number) {
+    return answer;
+}
+
+function findFactorialIterative(number){
+    let answer = 1;
+    if(number ===2){
+        answer = 2;
+    }
+    for(let i=2; i<=number; i++){
+        answer = answer * i;
+    }
+    return answer;
+}
+
+findFactorialIterative(5);
