340e1dcd0bf79315d0926fe9eca00ae577a21889
Jessica Yin <jyin978@hotmail.com>
2020-01-23 20:27:25 -0800

    solution 2
    added in solution 2 with while loop

--- codingProblems/LinkedLists/reverseLinkedList.js
+++ codingProblems/LinkedLists/reverseLinkedList.js
@@ -12,3 +12,23 @@ var reverseList = function reverseList(head, prev = null) {
     return next ? reverseList(next, head) : head;
 };
 
+//solution 2
+var reverseList = function reverseList(head) {
+     if (!head){
+         return head;
+     }
+    
+     let current = head.next;
+    
+     let node = head;
+     node.next = null;
+    
+     while(current){
+         let temp = current;
+         current = current.next
+         temp.next = node;
+         node = temp;
+     }
+    
+     return node;
+};
