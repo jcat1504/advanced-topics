5e209daa02df5d31e33e2d1ae5f4bd7c3219eaf0
Jessica Yin <jyin978@hotmail.com>
2020-02-21 11:31:43 -0800

    solution 2
    solution 2 for maximum sub array problem

--- codingProblems/Arrays/maximumSubArray.js
+++ codingProblems/Arrays/maximumSubArray.js
@@ -13,3 +13,15 @@ var maxSubArray = function(nums) {
     }
     return Math.max(...nums)
 }
+
+//solution 2
+function maxSubArray(A) {
+  var prev = 0;
+  var max = -Number.MAX_VALUE;
+
+  for (var i = 0; i < A.length; i++) {
+    prev = Math.max(prev + A[i], A[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
