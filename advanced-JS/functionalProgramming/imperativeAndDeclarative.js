b7a857f794b2b64d5b7376a379c24fced69fc8c3
Jessica Yin <jyin978@hotmail.com>
2020-05-01 16:07:35 -0700

    imperative and declarative examples.

---
+++ advanced-JS/functionalProgramming/imperativeAndDeclarative.js
@@ -0,0 +1,10 @@
+//imperative: tells the machine what to do and how to do it
+//declarative: tells machine what it should do and what should happen
+
+//imperative:
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+}
+
+//declarative: 
+[1, 2, 3].forEach(item => console.log(item));
