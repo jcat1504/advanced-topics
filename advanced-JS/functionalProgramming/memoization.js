eb8131f9790308077d7587c7303fc43d03292cb8
Jessica Yin <jyin978@hotmail.com>
2020-06-09 17:46:58 -0700

    memoization pt2
    changed up the function a bit to make 'cache' a private variable

--- advanced-JS/functionalProgramming/memoization.js
+++ advanced-JS/functionalProgramming/memoization.js
@@ -3,16 +3,22 @@ function addTo80(n) {
     return n + 80;
 }
 
-let cache = {};
-function memoizedAddTo80(n) {
-    if (n in cache) {
-        return cache[n];
-    } else {
-        console.log('long time');
-        cache[n] = n + 80;
-        return cache[n];
+
+function memoizedAddTo80() {
+        let cache = {};
+        return function(n){
+        if (n in cache) {
+            return cache[n];
+        } else {
+            console.log('long time');
+            cache[n] = n + 80;
+            return cache[n];
+        }
     }
 }
+//something about closures
 
-console.log('1', memoizedAddTo80(5));
-console.log('2', memoizedAddTo80(5));
+const memoized = memoizedAddTo80()
+
+console.log('1', memoized(5));
+console.log('2', memoized(5));
