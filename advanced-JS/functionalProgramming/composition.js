51848f1b45e482c07ace5efa747a77387ee27e9a
Jessica Yin <jyin978@hotmail.com>
2020-06-11 07:34:05 -0700

    learned about compose and pipe

---
+++ advanced-JS/functionalProgramming/composition.js
@@ -0,0 +1,12 @@
+//Compose
+//Pipe
+
+const compose = (f, g) => (data) => f(g(data))//reads from right to left
+const compose = (f, g) => (data) => g(f(data))//reads from left to right
+const multiplyBy3 = (num) => num*3;
+const makePositive = (num) => Math.abs(num);//all pure functions!
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive)
+
+multiplyBy3AndAbsolute(-50) //-50 is (data)
+
+//(multiplyby3) is f, (makePositive) is g
