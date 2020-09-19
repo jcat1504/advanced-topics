9b2a3260f1f2220de350acd5663c8ac8d5bd3507
Jessica Yin <jyin978@hotmail.com>
2020-09-18 17:13:45 -0700

    Update promises.js

--- advanced-JS/promises.js
+++ advanced-JS/promises.js
@@ -23,3 +23,18 @@ async function fetchUsers() {
     const data = await resp.json()
     console.log(data)
 }
+
+//PROMISE ALL
+Promise.all(urls.map(url =>
+.fetch(url).then(resp => resp.json())
+)).then(array => {
+    console.log('users', array[0])
+    console.log('posts', array[1])
+    console.log('albums', array[2])
+}).catch('oops')
+
+const getData = async function () {
+    const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+    fetch(url).then(resp => resp.json())
+    ))
+}
