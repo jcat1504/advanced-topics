c98752f8730e99d79ab87df510384c680cd65ab4
Jessica Yin <jyin978@hotmail.com>
2020-09-21 12:56:54 -0700

    Update promises.js

--- advanced-JS/promises.js
+++ advanced-JS/promises.js
@@ -26,15 +26,32 @@ async function fetchUsers() {
 
 //PROMISE ALL
 Promise.all(urls.map(url =>
-.fetch(url).then(resp => resp.json())
-)).then(array => {
-    console.log('users', array[0])
-    console.log('posts', array[1])
-    console.log('albums', array[2])
-}).catch('oops')
+    fetch(url).then(resp => resp.json())
+    )).then(array => {
+        console.log('users', array[0])
+        console.log('posts',  array[1])
+        console.log('albums',  array[2])
+    }).catch('oops')
+
 
 const getData = async function () {
+    try {
     const [ users, posts, albums ] = await Promise.all(urls.map(url =>
     fetch(url).then(resp => resp.json())
     ))
+    console.log('users', users)
+    console.log('posts', posts)
+    console.log('albums', albums)
+    } catch {
+        console.log('oops!')
+    }
 }
+
+//await for all
+
+const loopThroughUrls = urls => {
+    for (url of urls) {
+        console.log(urls)
+    }
+}
+//will loop through and iterate through each url and console log
