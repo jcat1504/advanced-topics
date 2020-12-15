a54e9ca8b5dddbe3f1a44d831fd666371c9d09df
Jessica Yin <jyin978@hotmail.com>
2020-12-14 19:43:04 -0800

    updating with optimal solution
    space: O(1)
    time: O(n)

--- codingProblems/Arrays/containerWithMostWater.js
+++ codingProblems/Arrays/containerWithMostWater.js
@@ -24,3 +24,22 @@ const getMaxWaterContainer = function(heights) {
     }
     return maxArea;
 }
+
+//OPTIMIZED SOLUTION
+
+const getMaxWaterContainer = function(heights) {
+    let p1=0, p2=height.length-1, maxArea = 0;
+    while (p1 < p2 ) {
+        const height = Math.min(heights[p1], heights[p2]);
+        const width = p2 - p1;
+        const area = height * width;
+        maxArea = Math.max(maxArea, area)
+        if(heights[p1] <= heights[p2]) {
+            p1++;
+        } else {
+            p2--;
+        }
+    }
+    return maxArea;
+}
+        
