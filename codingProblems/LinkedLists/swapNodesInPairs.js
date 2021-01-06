e0cc2861e78e92e67eb2121427b8ecac60bfd234
Jessica Yin <jyin978@hotmail.com>
2021-01-05 18:48:11 -0800

    solution 2
    using recursive function

--- codingProblems/LinkedLists/swapNodesInPairs.js
+++ codingProblems/LinkedLists/swapNodesInPairs.js
@@ -27,3 +27,12 @@ var swapPairs = function(head) {
     }
     return dummy.next 
 }
+
+//SOLUTION 2
+var swapPairs = function(head) {
+    if(!head || !head.next) return head;
+    var v1 = head, v2 = head.next, v3 = v2.next;
+    v2.next = v1
+    v1.next = swapPairs(v3)
+    return v2
+}
