6e04589144d92bc8f03589db20de4a35f462af8d
Jessica Yin <jyin978@hotmail.com>
2021-03-02 18:08:23 -0800

    Update majorityElement.js

--- codingProblems/HashTables/majorityElement.js
+++ codingProblems/HashTables/majorityElement.js
@@ -9,3 +9,10 @@ var majorityElement = function(nums) {
     }
     
 };
+
+//second solution
+var majorityElement = function(nums) {
+    // sort the array and the middle is the majority
+    nums.sort((a,b) => a - b);
+    return nums[Math.floor(nums.length/2)];
+}; 
