65654b16c2a381e2fa635f202eb78068d2c6780a
Jessica Yin <jyin978@hotmail.com>
2021-12-14 19:03:04 -0800

    solution 3

--- codingProblems/LinkedLists/copyListWithRandomPointer.js
+++ codingProblems/LinkedLists/copyListWithRandomPointer.js
@@ -52,3 +52,33 @@ var copyRandomList = function(head) {
     }
     return newHead;
 }
+
+if (head == null) {
+        return null;
+    }
+    let temp = head;
+    // first set the next pointers
+    while (temp !== null) {
+        let newNode = new Node(temp.val);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        temp = newNode.next;
+    }
+    // set the random pointers now
+    temp = head;
+    while (temp !== null) {
+        temp.next.random = temp.random ? temp.random.next : null;
+        temp = temp.next.next;
+    }
+    // unweave the list now
+    let oldList = head;
+    let newList = head.next;
+    let dummy = newList;
+    while (oldList !== null) {
+        oldList.next = oldList.next.next;
+        newList.next = newList.next ? newList.next.next : null;
+        oldList = oldList.next;
+        newList = newList.next;
+    }
+    return dummy;
+};
