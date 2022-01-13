502cbbc42442e6c8a31890660481beae6147c1e8
Jessica Yin <jyin978@hotmail.com>
2022-01-12 19:47:16 -0800

    recipe for min BST

--- Recipes/quickSort.js
+++ Recipes/quickSort.js
@@ -32,3 +32,43 @@ function quickSortRecursive(arr, start, end) {
     quickSortRecursive(arr, start, index - 1);
     quickSortRecursive(arr, index + 1, end);
 }
+
+//Iterative Implementation 
+function quickSortIterative(arr) {
+    //Creating an array that we'll use as a stack, using push and pop
+    stack = [];
+
+    // Adding the entire initial array as an "unsorted subarray"
+    stack.push(0)
+    stack.push(arr.length - 1);
+
+    //There isn't an explicit peek() function
+    //The loop repeats as long as we have unsorted subarrays 
+    while(stack[stack.length - 1] >= 0) { 
+        //Extracting the top unsorted subarray 
+        end = stack.pop();
+        start = stack.pop();
+
+        pivotIndex = partition(arr, start, end);
+
+        //If there are unsorted elements to the "left" of pivot,
+        //we add that subarray to the stack so we can sort it later 
+        if (pivotIndex - 1 > start) {
+            stack.push(start);
+            stack.push(pivotIndex - 1);
+        }
+
+        //If there are unsorted elements to the "right" of pivot,
+        // we add that subarray to the stack so we can sort it later 
+        if (pivotIndex + 1 < end) {
+            stack.push(pivotIndex + 1);
+            stack.push(end);
+        }
+    }
+}
+
+//testing 
+ourArray = [7, -2, 4, 1, 6, 5, 0, -4, 2]
+quickSortIterative(ourArray)
+
+console.log(ourArray)
